1566bd03c5d53704f4023ac87342da2a311718ec
Jeff <noojeff@gmail.com>
2018-02-24 12:57:26 +0000

    updated push func

--- push-to-gh-pages.js
+++ push-to-gh-pages.js
@@ -1,17 +1,19 @@
 const ghpages = require('gh-pages');
 const fs = require('fs-extra');
+const path = require('path');
+
+const dir_page = 'gh-pages';
 
 try {
     // Clear distination folder
-    fs.removeSync('gh-pages/');
+    fs.removeSync(dir_page);
 
     // Copy js
-    fs.copySync('dist', 'gh-pages/dist');
-
+    fs.copySync('dist', path.resolve(dir_page, 'dist'));
     // Copy index.html
-    fs.copySync('index.html', 'gh-pages/index.html')
+    fs.copySync('index.html', path.resolve(dir_page, 'index.html'));
 
-    ghpages.publish('gh-pages', function(err) {
+    ghpages.publish(dir_page, function(err) {
         console.log(err);
     });
 } catch (err) {
